ed6d530476fe528d50bf9c95fbeb5576361c16e8
SaiAshish <saiashish3760@gmail.com>
2022-12-31 02:11:27 +0530

    Add files via upload

---
+++ PrometheusAndGrafana/src/test/java/com/PrometheusAndGrafana/PrometheusAndGrafana/PrometheusAndGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrometheusAndGrafana.PrometheusAndGrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusAndGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
